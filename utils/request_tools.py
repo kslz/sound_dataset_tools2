864d5fd4f41d26a8ead49c679b97b433b2905fa8
李子 <49679880@qq.com>
2023-02-27 20:37:50 +0800

    编写标贝语音评测相关函数

--- utils/request_tools.py
+++ utils/request_tools.py
@@ -6,6 +6,7 @@
 """
 
 import requests
+import base64
 
 def get_biaobei_token(client_id,client_secret):
     url = 'https://openapi.data-baker.com/oauth/2.0/token'
@@ -24,3 +25,36 @@ def get_biaobei_token(client_id,client_secret):
         return access_token
     else:
         return False
+
+def pingce_biaobei(file_path, text, access_token):
+    """
+    通过标贝接口进行音频评测，并将返回的json文件保存到本地，需自行填入鉴权信息
+    注意：只支持采样率16k、位长16bit、单声道的pcm音频。
+    标贝语音评测只支持3秒以上的音频，建议使用wav2pcm2()函数进行转换
+    文档：https://www.data-baker.com/specs/file/eva_api_restful
+
+    :param file_path: 音频文件路径
+    :param text: 音频对应文本
+    :param access_token: 鉴权信息（请参考文档获取）
+    """
+
+    with open(file_path, "rb") as f:
+        base64_data = base64.b64encode(f.read()).decode("utf-8")
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Host': 'openapi.data-baker.com'
+    }
+
+    json_data = {
+        'access_token': access_token,
+        'format': 'pcm',
+        'txt': text,
+        'lan': 'cn',
+        'audio': base64_data,
+    }
+
+    response = requests.post('https://openapi.data-baker.com/cap/getCapScore', headers=headers, json=json_data)
+
+    response_json = response.content.decode("utf-8")
+    print(response_json)
