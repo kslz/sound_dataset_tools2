08cb2a03f2dbd430e6b120b62c8cd7f9772d2b11
李子 <49679880@qq.com>
2023-03-08 15:42:13 +0800

    编译

--- 编译.bat
+++ 编译.bat
@@ -1,1 +1,9 @@
 pyinstaller main.py -F -n sound_dataset_tools2
+
+
+pip install nuitka
+pip install ordered-set
+pip install zstandard
+
+
+nuitka --standalone main.py --enable-plugin=pyside6 --onefile --output-dir=out
