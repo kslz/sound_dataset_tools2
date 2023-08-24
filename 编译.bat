a2c5bb342b28a888ea7d2bebc77b14632ea36fec
李子 <49679880@qq.com>
2023-08-24 10:07:35 +0800

    v2.0重构删除

--- 编译.bat
+++
@@ -1,9 +0,0 @@
-pyinstaller main.py -F -n sound_dataset_tools2
-
-
-pip install nuitka
-pip install ordered-set
-pip install zstandard
-
-
-nuitka --standalone main.py --enable-plugin=pyside6 --onefile --output-dir=out
