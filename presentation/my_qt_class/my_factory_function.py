f2c13d7cbbe9bee9036ca47c0dfe9f17368abba3
lizi <49679880@qq.com>
2023-09-16 22:41:01 +0800

    优化操作列编辑按钮

--- presentation/my_qt_class/my_factory_function.py
+++ presentation/my_qt_class/my_factory_function.py
@@ -8,10 +8,22 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QSizePolicy
 
 
-def make_operate_btns(parent, data_list: list, length_list=None):
+def make_operate_btns(parent, data_list: list):
     """
-    生成表格中的编辑列按钮组
+    生成表格中的操作列按钮组（只会生成QPushButton）
 
+    例：data_list = [
+        {'text': '进入', 'slot': lambda: self.open_dataset_window(dataset_id)},
+        {'text': '编辑', 'slot': lambda: self.edit_dataset(dataset_id)},
+        {'text': '删除', 'slot': lambda: self.del_dataset(dataset_id, dataset_name)},
+    ]
+
+
+    :param parent:
+    :param data_list: 格式为[['text':"按钮名",'slot':要绑定的槽函数,'length':指定的按钮宽度(不填则默认为一个字占用1比例单位)]...]
+    :param btns:
+    :param length_list:
+    :return:
     """
 
     caozuo_widget = QWidget()
@@ -20,21 +32,55 @@ def make_operate_btns(parent, data_list: list, length_list=None):
     layout.setSpacing(1)
     caozuo_widget.setLayout(layout)
 
-    if length_list is None:
-        length_list = []
-        for data in data_list:
-            length_list.append(len(data[0]))
-
-    for i, data in enumerate(data_list):
-        btn = QPushButton(data[0])
-
+    for data in data_list:
+        data: dict
+        btn = QPushButton(data['text'], parent=parent)
         # 创建一个QSizePolicy对象，并设置水平拉伸因子
         btn.setMinimumWidth(1)
         size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        size_policy.setHorizontalStretch(length_list[i])
+        size_policy.setHorizontalStretch(data.get('length', len(data['text'])))
         btn.setSizePolicy(size_policy)
+        btn.clicked.connect(data['slot'])
+        layout.addWidget(btn)
 
-        btn.clicked.connect(data[1])
+    return caozuo_widget
+
+
+def make_my_operate_btns(parent, data_list: list):
+    """
+    生成表格中的操作列按钮组（需要传入按钮类型，支持自定义按钮）
+
+    例：data_list = [
+        {'btn': AudioButton,'args': {'wav_path': info_file_path, 'start_time': info_start_time, 'end_time': info_end_time,'parent': self}, 'slot': None, 'length': 2},
+        {'btn': QPushButton, 'args': {'text': '快速导出', 'parent': self}, 'slot': lambda: self.fast_output(info_id)},
+        {'btn': QPushButton, 'args': {'text': '编辑', 'parent': self}, 'slot': lambda: self.edit_info(info_id)},
+        {'btn': QPushButton, 'args': {'text': '删除', 'parent': self}, 'slot': lambda: self.del_info(info_id, info_is_del)},
+    ]
+
+
+    :param parent:
+    :param data_list: 格式为[['text':"按钮名",'slot':要绑定的槽函数,'length':指定的按钮宽度(不填则默认为一个字占用1比例单位)]...]
+    :param btns:
+    :param length_list:
+    :return:
+    """
+
+    caozuo_widget = QWidget()
+    layout = QHBoxLayout()
+    layout.setContentsMargins(0, 0, 0, 0)
+    layout.setSpacing(1)
+    caozuo_widget.setLayout(layout)
+
+    for data in data_list:
+        data: dict
+        btn = data['btn'](**data['args'])
+        # 创建一个QSizePolicy对象，并设置水平拉伸因子
+        btn.setMinimumWidth(1)
+        size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        size_policy.setHorizontalStretch(data.get('length', len(data['args'].get('text', "默认"))))
+        btn.setSizePolicy(size_policy)
+        if data['slot']:
+            btn.clicked.connect(data['slot'])
         layout.addWidget(btn)
 
     return caozuo_widget
